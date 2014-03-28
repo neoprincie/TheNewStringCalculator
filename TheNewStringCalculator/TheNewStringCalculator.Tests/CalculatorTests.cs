dc2acad28307f21d520aa652f9ebf37105fb77c0
Kevin Poston <kevposton@gmail.com>
2014-03-27 21:26:06 -0400

    Created Calculator and passed initial tests.

---
+++ TheNewStringCalculator/TheNewStringCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TheNewStringCalculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator evaluator;
+
+        [SetUp]
+        public void Setup()
+        {
+            evaluator = new Calculator();
+        }
+
+        [Test]
+        public void TestBaseCase()
+        {
+            var input = "1";
+
+            var result = evaluator.Calculate(input);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestNegativeBaseCase()
+        {
+            var input = "-1";
+
+            var result = evaluator.Calculate(input);
+
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [TestCase("1+1", 2)]
+        [TestCase("1*1", 1)]
+        [TestCase("4/2", 2)]
+        [TestCase("4-2", 2)]
+        [TestCase("5%2", 1)]
+        [TestCase("1*(2+4)", 6)]
+        [TestCase("2*(3+2*3)", 18)]
+        [TestCase("(3+(5-2))*2", 12)]
+        public void TestRecursiveCase(String expression, Double expected)
+        {
+            var result = evaluator.Calculate(expression);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase("-1+3", 2)]
+        [TestCase("3+-1", 2)]
+        [TestCase("3--1", 4)]
+        public void TestAddingNegatives(String expression, Double expected)
+        {
+            var result = evaluator.Calculate(expression);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestMultipleExpression()
+        {
+            var input = "1+2*4";
+
+            var result = evaluator.Calculate(input);
+
+            Assert.That(result, Is.EqualTo(9));
+        }
+    }
+}
---
+++ TheNewStringCalculator/TheNewStringCalculator/Calculator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheNewStringCalculator
+{
+    public class Calculator
+    {
+        private Dictionary<String, Func<Double, Double, Double>> operators;
+
+        public Calculator()
+        {
+            operators = new Dictionary<String, Func<Double, Double, Double>>();
+            operators.Add("^", (x, y) => Math.Pow(x, y));
+            operators.Add("*", (x, y) => x * y);
+            operators.Add("/", (x, y) => x / y);
+            operators.Add("%", (x, y) => x % y);
+            operators.Add("+", (x, y) => x + y);
+            operators.Add("-", (x, y) => x - y);
+        }
+
+        public Double Calculate(String expression)
+        {
+            var expressionWithoutParentheses = EvaluateAnyParentheses(expression);
+            
+            return Evaluate(expressionWithoutParentheses);
+        }
+
+        private String EvaluateAnyParentheses(String expression)
+        {
+            if (!expression.Contains(")"))
+                return expression;
+
+            var closeParenIndex = expression.IndexOf(")");
+            var beginParenIndex = GetBeginningParenthesesIndex(expression, closeParenIndex);
+            var parenExpression = expression.Substring(beginParenIndex + 1, closeParenIndex - beginParenIndex - 1);
+            var result = Evaluate(parenExpression).ToString();
+            expression = expression.Replace("(" + parenExpression + ")", result);
+            
+            return EvaluateAnyParentheses(expression);
+        }
+
+        private Int32 GetBeginningParenthesesIndex(String expression, Int32 closeParenIndex)
+        {
+            var beginParenIndex = 0;
+
+            for (var i = closeParenIndex; i > 0; i--)
+            {
+                if (expression[i] == '(')
+                {
+                    beginParenIndex = i;
+                    break;
+                }
+            }
+
+            return beginParenIndex;
+        }
+
+        private Double Evaluate(String expression)
+        {
+            Double num;
+
+            if (Double.TryParse(expression, out num))
+            {
+                return num;
+            }
+            else
+            {
+                expression = expression.Replace("--", "+");
+                var op = GetLowestOperator(expression);
+                var parts = expression.Split(op.ToCharArray()[0]);
+
+                return operators[op](Evaluate(parts[0]), Evaluate(parts[1]));
+            }
+        }
+
+        private String GetLowestOperator(String expression)
+        {
+            if (expression.Contains("+"))
+                return "+";
+            if (expression.Contains("-"))
+                return "-";
+            if (expression.Contains("%"))
+                return "%";
+            if (expression.Contains("/"))
+                return "/";
+            if (expression.Contains("*"))
+                return "*";
+            if (expression.Contains("^"))
+                return "^";
+
+            return null;
+        }
+    }
+}
